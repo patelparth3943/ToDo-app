7df107bcbfc2b42c391e3bf1618657a9f4ebcdd0
parth <patelparth3943@gmail.com>
2024-02-22 16:50:37 +0530

    done

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -16,58 +16,97 @@ export default class extends Component {
 
   inputRef = createRef();
 
-  addTodo = event => {
-    event.preventDefault();
-    const inputText = this.inputRef.current;
-    this.setState(
-      ({ todoList }) => ({
-        todoList: [
-          ...todoList,
-          {
-            id: uuidv4(),
-            text: inputText.value,
-            isDone: false,
-          },
-        ],
-      }),
-      () => {
-        inputText.value = '';
-      },
-    );
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://localhost:3000/todoList');
+      const json = await res.json();
+      this.setState({ todoList: json })
+    } catch (error) {
+
+    }
+  }
+
+  addTodo = async event => {
+    try {
+      event.preventDefault();
+      const inputText = this.inputRef.current;
+
+      const res = await fetch('http://localhost:3000/todoList', {
+        method: "POST",
+        body: JSON.stringify({
+          text: inputText.value,
+          isDone: false,
+        }),
+        headers: {
+          'Content-Type': 'application.json',
+          Accept: 'application.json'
+        }
+      });
+      const json = await res.json();
+
+      this.setState(
+        ({ todoList }) => ({
+          todoList: [
+            ...todoList, json],
+        }),
+        () => {
+          inputText.value = '';
+        },
+      );
+    } catch (error) { }
   };
 
-  toggleComplete = item => {
-    console.log('toggleComplete');
-    this.setState(({ todoList }) => {
-      const index = todoList.findIndex(x => x.id === item.id);
-      return {
-        todoList: [
-          ...todoList.slice(0, index),
-          { ...item, isDone: !item.isDone },
-          ...todoList.slice(index + 1),
-        ],
-      };
-    });
+
+  toggleComplete = async item => {
+    try {
+      const res = await fetch(`http://localhost:3000/todoList/${item.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          ...item,
+          isDone: !item.isDone,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      const json = await res.json();
+
+      this.setState(({ todoList }) => {
+        const index = todoList.findIndex(x => x.id === item.id);
+        return {
+          todoList: [
+            ...todoList.slice(0, index),
+            json,
+            ...todoList.slice(index + 1),
+          ],
+        };
+      });
+    } catch (error) { }
   };
 
-  deleteTodo = item => {
-    this.setState(({ todoList }) => {
-      const index = todoList.findIndex(x => x.id === item.id);
-      return {
-        todoList: [
-          ...todoList.slice(0, index),
-          ...todoList.slice(index + 1)],
-      };
-    });
+  deleteTodo = async item => {
+    try {
+      await fetch(`http://localhost:3000/todoList/${item.id}`, {
+        method: 'DELETE',
+      });
+
+      this.setState(({ todoList }) => {
+        const index = todoList.findIndex(x => x.id === item.id);
+        return {
+          todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
+        };
+      });
+    } catch (error) { }
   };
 
   changeFilterType = filterType => {
     this.setState({ filterType });
   };
 
-
   render() {
-    console.log('render');
+    // console.log('render');
     const { todoList, filterType } = this.state
     return (
       <div className="flex min-h-screen flex-col items-center gap-8 pt-5">
